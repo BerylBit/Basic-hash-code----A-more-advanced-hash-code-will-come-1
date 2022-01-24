1d393ce7303e2fa48950f57c48dc8a78f31772c3
BerylBit <95605576+BerylBit@users.noreply.github.com>
2022-01-24 14:01:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const lightningHash = (data) => {
+  return data + '*';
+}
+
+class Block {
+  constructor(data, hash, lastHash) {
+     this.data = data;
+     this.hash = hash;
+     this.lastHash = lastHash;
+  }
+}
+
+class Blockchain {
+  constructor() {
+    const genesis = new  Block('gen-data', 'gen-hash', 'gen-lastHash');
+
+    this.chain = [genesis];
+  }
+
+  addBlock(data) {
+    const lastHash = this.chain[this.chain.length-1].hash;
+
+    const hash = lightningHash(data + lastHash);
+
+    const block = new Block(data, hash, lastHash);
+
+    this.chain.push(block);
+  }
+}
+
+const BRBBlockchain = new Blockchain();
+BRBBlockchain.addBlock('one');
+BRBBlockchain.addBlock('two');
+BRBBlockchain.addBlock('three');
+
+console.log(BRBBlockchain);
